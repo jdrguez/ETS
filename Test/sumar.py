62ab9a78eef2290f29dc568eccf6515ceb4d08bc
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-04-18 15:08:30 +0100

    Se

---
+++ Test/sumar.py
@@ -0,0 +1,2 @@
+def sum_number(num1: int, num2: int) -> int:
+    return num1 + num2 
---
+++ Test/test_suma.py
@@ -0,0 +1,5 @@
+import pytest 
+from sumar.py import sum_number
+
+def test_sum_number():
+    assert sum_number(2, 2) == 4
